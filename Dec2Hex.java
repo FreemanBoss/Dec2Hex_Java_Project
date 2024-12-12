91be140d935c859351a30a3fa44bead7ecd71634
FreemanBoss <bossfreeman9@gmail.com>
2024-12-12 06:28:47 +0000

    feat: add error handling for missing and non-integer inputs in Dec2Hex.java

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -2,21 +2,39 @@ import java.util.Scanner;
 
 class Dec2Hex {
     public static int Arg1;
+
     public static void main(String args[]) {
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal = "";
+        try {
+            // Check if an argument is provided
+            if (args.length == 0) {
+                throw new IllegalArgumentException("Error: No input argument provided.");
+            }
 
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
+            // Parse the input argument as an integer
+            Arg1 = Integer.parseInt(args[0]);
 
-        while(num != 0) {
-            rem = num % 16;
-            hexadecimal = ch[rem] + hexadecimal;
-            num = num / 16;
+            // Start converting to hexadecimal
+            char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+            int rem, num;
+            num = Arg1;
+            String hexadecimal = "";
+
+            System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
+            while (num != 0) {
+                rem = num % 16;
+                hexadecimal = ch[rem] + hexadecimal;
+                num = num / 16;
+            }
+
+            System.out.println("Hexadecimal representation is: " + hexadecimal);
+        
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: No input argument provided.");
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Input argument is not a valid integer.");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
-
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
     }
 }
